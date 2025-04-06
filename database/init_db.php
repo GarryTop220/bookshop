edb33df1cc811af5440f08a40717755f19b1a95f
ifsmth <annuk.pavlo@gmail.com>
2025-04-06 20:55:59 +0300

    changes

--- database/init_db.php
+++ database/init_db.php
@@ -95,7 +95,7 @@ CREATE TABLE IF NOT EXISTS order_details (
 );
 
 INSERT IGNORE INTO users (name, email, password, role) VALUES 
-('admin', 'admin@mail.com', 'admin', 'admin');
+('admin', 'admin@mail.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin');
 
 INSERT IGNORE INTO genres (name) VALUES 
 ('Детективи'), 

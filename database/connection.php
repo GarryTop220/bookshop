5a04b8f80fc5fe4a9fc7d424bfcc412993791379
ifsmth <annuk.pavlo@gmail.com>
2025-04-06 19:07:57 +0300

    changes

--- database/connection.php
+++ database/connection.php
@@ -1,9 +1,11 @@
 <?php
-	$HOST = 'localhost';
-	$USER = 'root';
-	$PASS = 'aXGnEnpUJZUgJHBZTNSOlpisswEMAtCr';
-	$DB = 'railway';
-	$conn = mysqli_connect($HOST,$USER,$PASS,$DB)
-        or die("Couldn't connect to database");
-	mysqli_set_charset($conn,"utf8");
+	$HOST = getenv('MYSQLHOST');      // або $_ENV['MYSQLHOST']
+	$USER = getenv('MYSQLUSER');      // або $_ENV['MYSQLUSER']
+	$PASS = getenv('MYSQLPASSWORD');  // або $_ENV['MYSQLPASSWORD']
+	$DB   = getenv('MYSQLDATABASE');  // або $_ENV['MYSQLDATABASE']
+	$PORT = getenv('MYSQLPORT');      // або $_ENV['MYSQLPORT']
+
+	$conn = mysqli_connect($HOST, $USER, $PASS, $DB, $PORT)
+		or die("Connection error: " . mysqli_connect_error());
+	mysqli_set_charset($conn, "utf8");
 ?>

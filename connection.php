a79b40ff8a90f5bb5823b89706f9a511f0fb67e6
ifsmth <annuk.pavlo@gmail.com>
2025-04-06 20:00:59 +0300

    changes

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
-	$HOST = getenv('MYSQLHOST');      // або $_ENV['MYSQLHOST']
-	$USER = getenv('MYSQLUSER');      // або $_ENV['MYSQLUSER']
-	$PASS = getenv('MYSQLPASSWORD');  // або $_ENV['MYSQLPASSWORD']
-	$DB   = getenv('MYSQLDATABASE');  // або $_ENV['MYSQLDATABASE']
-	$PORT = getenv('MYSQLPORT');      // або $_ENV['MYSQLPORT']
+	$HOST = 'mysql.railway.internal';      // або $_ENV['MYSQLHOST']
+	$USER = 'root';      // або $_ENV['MYSQLUSER']
+	$PASS = 'aXGnEnpUJZUgJHBZTNSOlpisswEMAtCr';  // або $_ENV['MYSQLPASSWORD']
+	$DB   = 'railway';  // або $_ENV['MYSQLDATABASE']
+	$PORT = '3306';      // або $_ENV['MYSQLPORT']
 
 	$conn = mysqli_connect($HOST, $USER, $PASS, $DB, $PORT)
 		or die("Connection error: " . mysqli_connect_error());
